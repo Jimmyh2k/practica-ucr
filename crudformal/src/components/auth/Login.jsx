bbbf12db2ae0119f9b0c048f68b49c0f84a36612
Braulio <braulio@focusgte.com>
2022-06-04 01:28:35 -0600

    Adición de estilos material a login

--- crudformal/src/components/auth/Login.jsx
+++ crudformal/src/components/auth/Login.jsx
@@ -1,7 +1,10 @@
-import React, {useState, useContext} from "react";
+import React, { useState, useContext } from "react";
 import Swal from 'sweetalert2';
 import clienteAxios from '../../config/axios';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { Box, TextField, Button, Container, Typography, Avatar } from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
+
 
 // Context
 import { CRMContext } from '../../context/CRMContext';
@@ -11,23 +14,23 @@ function Login() {
     // Auth y token
     const [auth, guardarAuth] = useContext(CRMContext);
 
-    const [ credenciales, guardarCredenciales] = useState({});
+    const [credenciales, guardarCredenciales] = useState({});
     const navigate = useNavigate();
 
     const iniciarSesion = async e => {
         e.preventDefault();
 
         //Autentica al usuario
-        try{
-            const respuesta = await clienteAxios.post('/iniciar-sesion',credenciales);
+        try {
+            const respuesta = await clienteAxios.post('/iniciar-sesion', credenciales);
 
             // extraer el token y colocarlo en localstorage
             const { token } = respuesta.data;
             localStorage.setItem('token', token);
 
-             // colocarlo en el state
-             guardarAuth({
-                token, 
+            // colocarlo en el state
+            guardarAuth({
+                token,
                 auth: true
             })
 
@@ -38,55 +41,109 @@ function Login() {
             )
             navigate('/');
 
-        } catch (error){
+        } catch (error) {
             console.log(error);
-            Swal.fire({type:'error', title:'Hubo un error',
-            text: error.response.data.mensaje
-        })
+            Swal.fire({
+                type: 'error', title: 'Hubo un error',
+                text: error.response.data.mensaje
+            })
         }
     }
 
     const leerDatos = e => {
         guardarCredenciales({
             ...credenciales,
-            [e.target.name] : e.target.value
+            [e.target.name]: e.target.value
         })
     }
 
-    return(
-        <div className="login">
-            <h2>Iniciar Sesión</h2>
-
-            <div className="contenedor-formulario">
-                <form onSubmit={iniciarSesion}>
-
-                    <div className="campo">
-                        <label>Email</label>
-                        <input 
-                            type="text"
-                            name="correo"
-                            placeholder="Correo Electronico"
-                            required
-                            onChange={leerDatos}
-                        />
-                    </div>
-
-                    <div className="campo">
-                        <label>Password</label>
-                        <input 
-                            type="password"
-                            name="contrasena"
-                            placeholder="Contraseña"
-                            required
-                            onChange={leerDatos}
-                        />
-                    </div>
-
-                    <input type="submit" value="Iniciar Sesión" className="btn btn-verde btn-block" />
-                </form>
-            </div>
-        </div>
+    return (
+        <Container component="main" maxWidth="xs">
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '10px',
+                    alignItems: 'center',
+                    backgroundColor: '#ffffff',
+                    padding: '30px',
+                    borderRadius: '10px'
+                }}
+            >
+                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Iniciar Sesión
+                </Typography>
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                    autoFocus
+                    onChange={leerDatos}
+                />
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    onChange={leerDatos}
+                />
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                >              Iniciar Sesión
+                </Button>
+            </Box>
+        </Container >
     );
 }
 
 export default Login;
+
+
+
+        // <div className="login">
+        //     <h2>Iniciar Sesión</h2>
+
+        //     <div className="contenedor-formulario">
+        //         <form onSubmit={iniciarSesion}>
+
+        //             <div className="campo">
+        //                 <label>Email</label>
+        //                 <input
+        //                     type="text"
+        //                     name="correo"
+        //                     placeholder="Correo Electronico"
+        //                     required
+        //                     onChange={leerDatos}
+        //                 />
+        //             </div>
+
+        //             <div className="campo">
+        //                 <label>Password</label>
+        //                 <input
+        //                     type="password"
+        //                     name="contrasena"
+        //                     placeholder="Contraseña"
+        //                     required
+        //                     onChange={leerDatos}
+        //                 />
+        //             </div>
+
+        //             <input type="submit" value="Iniciar Sesión" className="btn btn-verde btn-block" />
+        //         </form>
+        //     </div>
+        // </div>
