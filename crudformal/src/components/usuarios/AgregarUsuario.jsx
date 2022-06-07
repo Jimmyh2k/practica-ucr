e0a3c2557002926721103a101edd138856869ad2
Jimmy Carballo <jcarballoh2k@gmail.com>
2022-06-07 01:15:43 -0600

    .

--- crudformal/src/components/usuarios/AgregarUsuario.jsx
+++ crudformal/src/components/usuarios/AgregarUsuario.jsx
@@ -1,6 +1,6 @@
 import React, {Fragment, useState} from "react";
 import clienteAxios from "../../config/axios";
-import {useNavigate,} from 'react-router-dom'
+import {useNavigate} from 'react-router-dom'
 import Swal from "sweetalert2";
 
 function AgregarUsuario(){
