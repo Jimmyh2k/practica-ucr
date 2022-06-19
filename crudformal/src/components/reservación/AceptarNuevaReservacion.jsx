6a169616f423fdc8f66dbc052c1c951c8cca9e76
Braulio <braulio@focusgte.com>
2022-06-19 00:38:01 -0600

    Cambios en estilo

--- crudformal/src/components/reservación/AceptarNuevaReservacion.jsx
+++ crudformal/src/components/reservación/AceptarNuevaReservacion.jsx
@@ -1,23 +1,34 @@
 import React, { useContext } from "react";
 import { FacturaContext } from '../../context/FacturaContext';
-import { Paper, Typography, Button } from '@mui/material'
+import { Paper, Typography, Button, TextField } from '@mui/material'
 
 const AceptarNuevaReservacion = () => {
 
-    const { cantidadDePersonas, checkIn, checkOut, clienteReservacion, habitacionReservacion, seleccionDeCliente, seleccionDeHabitacion, dataForUI } = useContext(FacturaContext);
+    const { checkIn, checkOut, reservacion, dataForUI, actualizarReservacion } = useContext(FacturaContext);
     return (
         <Paper sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '1.5rem', mt: '2rem' }}>
-            <Typography sx={{ mt: '2rem' }} variant="h6" component="h2">
+            <Typography sx={{ mt: '2rem' }} variant="h5" component="h2">
                 Ha seleccionado los siguientes datos.
             </Typography>
             <Typography variant="subtitle2" component="h2">
-                Si desea crear una nueva reservación presione Aceptar.
+                Si desea comenzar de nuevo presione Reset
             </Typography>
             <Typography>Cliente: {dataForUI.datosDelCliente.nombre}</Typography>
-            <Typography>Cantidad de personas: {cantidadDePersonas}</Typography>
+            <Typography>Cantidad de personas: {reservacion.cantidadDePersonas}</Typography>
             <Typography>Fecha de checkIn: { } {checkIn.getDate()} - {checkIn.getMonth()} - {checkIn.getFullYear()} </Typography>
             <Typography>Fecha de checkOut: { } {checkOut.getDate()} - {checkOut.getMonth()} - {checkOut.getFullYear()} </Typography>
             <Typography>Número de habitación: {dataForUI.datosDeHabitacion.numero}</Typography>
+            <TextField
+                id="comentarios"
+                name="comentarios"
+                label="Comentarios"
+                multiline
+                rows={4}
+                variant="outlined"
+                value={reservacion.comentarios}
+                onChange={e => (actualizarReservacion({ name: e.target.name, value: e.target.value }))}
+
+            />
             <Button sx={{ mb: '2rem' }} variant="outlined">Crear reservacion</Button>
         </Paper>
     )
--- crudformal/src/components/reservación/SeleccionarFechas.jsx
+++ crudformal/src/components/reservación/SeleccionarFechas.jsx
@@ -10,38 +10,41 @@ import DatePicker from 'react-date-picker';
 
 function SeleccionarFechas() {
 
-    const { checkIn, setCheckIn, checkOut, setCheckOut, actualizarReservacion } = useContext(FacturaContext);
+    const { checkIn, setCheckIn, checkOut, setCheckOut, reservacion, actualizarReservacion } = useContext(FacturaContext);
     return (
         <Paper sx={{
             display: 'flex', flexDirection: 'column', gap: '1rem', alignItems: 'center', textAlign: 'center', height: '100%'
         }}>
             <TextField
+                sx={{ mt: '4rem' }}
                 margin="normal"
                 required
                 name="cantidadDePersonas"
                 label="Cantidad de personas"
-                type="text"
+                type="number"
                 id="cantidadDePersonas"
+                InputProps={{ inputProps: { min: 0 } }}
+                value={reservacion.cantidadDePersonas}
                 onChange={e => actualizarReservacion({ name: e.target.name, value: e.target.value })}
             />
 
             <Box sx={{ display: { xs: 'none', lg: 'flex' }, flexDirection: 'row', alignItems: 'center', justifyContent: 'center', gap: '1rem' }}>
                 <Box sx={{}}>
-                    <Typography variant="h5" component="h2">Seleccione el Check In</Typography>
+                    <Typography variant="h6" component="h2">Seleccione el Check In</Typography>
                     <Calendar onChange={setCheckIn} value={checkIn} />
                 </Box>
                 <Box sx={{}}>
-                    <Typography variant="h5" component="h2">Seleccione el Check Out</Typography>
+                    <Typography variant="h6" component="h2">Seleccione el Check Out</Typography>
                     <Calendar onChange={setCheckOut} value={checkOut} />
                 </Box>
             </Box>
             <Box sx={{ display: { xs: 'flex', lg: 'none' }, flexDirection: { xs: 'column' }, alignItems: 'center', justifyContent: 'center', gap: '4rem', pt: { md: '4rem' } }}>
                 <Box sx={{}}>
-                    <Typography variant="h5" component="h2">Seleccione el Check In</Typography>
+                    <Typography variant="h6" component="h2">Check In</Typography>
                     <DatePicker onChange={setCheckIn} value={checkIn} />
                 </Box>
                 <Box sx={{}}>
-                    <Typography variant="h5" component="h2">Seleccione el Check Out</Typography>
+                    <Typography variant="h6" component="h2">Check Out</Typography>
                     <DatePicker onChange={setCheckOut} value={checkOut} />
                 </Box>
             </Box>
