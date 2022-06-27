584f4843e7f8d2194f3a6b880c58ed51901a5253
Braulio <braulio@focusgte.com>
2022-06-27 16:08:45 -0600

    Estilo en listar facturas

--- crudformal/src/components/factura/Factura.jsx
+++ crudformal/src/components/factura/Factura.jsx
@@ -1,26 +1,129 @@
 import React from "react";
+import { ListItem, Box, Typography, Paper, Card, CardContent, CardActions, Button, TableRow, TableCell, Grid } from '@mui/material';
+import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
+import { Link } from 'react-router-dom'
+import SimCardDownloadOutlinedIcon from '@mui/icons-material/SimCardDownloadOutlined';
 
-function Factura(props){
-//console.log(props.factura.nombre); Consultar por manera mas sencilla de usar props
+function Factura(props) {
+    //console.log(props.factura.nombre); Consultar por manera mas sencilla de usar props
     //Extraer los valores
-    const {idFactura, fechaEmision, condicionVenta, MedioPago,totalVenta, idReservacion,reservacion} = props.factura;
+    const { idFactura, fechaEmision, condicionVenta, MedioPago, totalVenta, idReservacion, reservacion } = props.factura;
     console.log(props.factura.reservacion.cliente.nombre);
+    const fechaConFormato = (fechaSinFormato) => {
+        const date = new Date(fechaSinFormato);
+        return `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`
+    }
+    const nombreCondicionDeVenta = (codigoCondicionVenta) => {
+        switch (codigoCondicionVenta) {
+            case '01':
+                return 'Contado'
+                break;
+            case '02':
+                return 'Crédito'
+                break;
+            case '03':
+                return 'Consignación'
+                break;
+            case '04':
+                return 'Apartado'
+                break;
+            case '05':
+                return 'Arrendamiento con opción de compra'
+                break;
+            case '06':
+                return 'Arrendamiento en función financiera'
+                break;
+            case '07':
+                return 'Cobro a favor de un tercero'
+                break;
+            case '08':
+                return 'Servicios prestados al Estado a crédito'
+                break;
+            case '09':
+                return 'Pago del servicios prestado al Estado'
+                break;
+            default:
+                break;
+        }
+    }
 
-
-    return(
-        <li className="cliente">
-                    <div className="info-cliente">
-                        <p className="nombre">Nombre del cliente:{reservacion.cliente.nombre}</p>
-                        <p className="empresa">Fecha de Emisión: {fechaEmision}</p>
-                        <p className="empresa">Condicion Venta: {condicionVenta}</p>
-                        <p className="empresa">Medio de Pago:{MedioPago}</p>
-                        <p className="empresa">Total Venta: {totalVenta}</p>
-                        <p className="empresa">idReservacion:{idReservacion}</p>
-                        <p className="empresa">Habitacion Pagada:{reservacion.habitacion.numero}</p>
-                    </div>
-
-                </li>
-    )
+    if (props.card) {
+        return (
+            <Grid item xs={6} >
+                <Card sx={{}}>
+                    <CardContent>
+                        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'left', gap: '0.5rem' }}>
+                            <Typography variant="h6" component="h3">
+                                Fecha de emisión
+                            </Typography>
+                            <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'left', gap: '0.5rem' }}>
+                                <CalendarMonthOutlinedIcon />
+                                <Typography variant="subtitle1" component="h3">
+                                    {fechaConFormato(fechaEmision)}
+                                </Typography>
+                            </Box>
+                        </Box>
+                        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'right', gap: '0.5rem' }}>
+                            <Typography variant="h6" component="h3">
+                                Total venta
+                            </Typography>
+                            <Typography variant="subtitle1" component="h3">
+                                {totalVenta}
+                            </Typography>
+                        </Box>
+                        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'right', gap: '0.5rem' }}>
+                            <Typography variant="h6" component="h3">
+                                Condición Venta
+                            </Typography>
+                            <Typography variant="subtitle1" component="h3">
+                                {nombreCondicionDeVenta(condicionVenta)}
+                            </Typography>
+                        </Box>
+                        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'right', gap: '0.5rem' }}>
+                            <Typography variant="h6" component="h3">
+                            </Typography>
+                        </Box>
+                        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'right', gap: '0.5rem' }}>
+                            <Typography variant="h6" component="h3">
+                            </Typography>
+                        </Box>
+                        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'right', gap: '0.5rem' }}>
+                            <Typography variant="h6" component="h3">
+                            </Typography>
+                        </Box>
+                        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'right', gap: '0.5rem' }}>
+                            <Typography variant="h6" component="h3">
+                            </Typography>
+                        </Box>
+                        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'right', gap: '0.5rem' }}>
+                            <Typography variant="h6" component="h3">
+                            </Typography>
+                        </Box>
+                    </CardContent>
+                    <CardActions>
+                        <Button component={Link} to={''} size="small" sx={{ marginRight: 1 }}>
+                            Ver Detalles
+                        </Button>
+                        <Button startIcon={<SimCardDownloadOutlinedIcon />} size="small" onClick={''}>
+                            XML
+                        </Button>
+                    </CardActions>
+                </Card>
+            </Grid>
+        )
+    }
 }
 
 export default Factura;
+{/* <li className="cliente">
+    <div className="info-cliente">
+        <p className="nombre">Nombre del cliente:{reservacion.cliente.nombre}</p>
+        <p className="empresa">Fecha de Emisión: {fechaEmision}</p>
+        <p className="empresa">Condicion Venta: {condicionVenta}</p>
+        <p className="empresa">Medio de Pago:{MedioPago}</p>
+        <p className="empresa">Total Venta: {totalVenta}</p>
+        <p className="empresa">idReservacion:{idReservacion}</p>
+        <p className="empresa">Habitacion Pagada:{reservacion.habitacion.numero}</p>
+    </div>
+
+</li> */}
--- crudformal/src/components/factura/Facturas.jsx
+++ crudformal/src/components/factura/Facturas.jsx
@@ -1,74 +1,108 @@
-import React,{useEffect, useState, Fragment, useContext} from "react";
+import React, { useEffect, useState, Fragment, useContext } from "react";
 import clienteAxios from '../../config/axios';
 import Factura from "./Factura";
-import {Link, useNavigate} from 'react-router-dom';
-
+import { Link, useNavigate } from 'react-router-dom';
+import { Typography, Button, Box, List, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Grid, Card, CardActions, CardContent } from '@mui/material';
 // import el Context
+import AddCircleOutlinedIcon from '@mui/icons-material/AddCircleOutlined';
+
 import { CRMContext } from '../../context/CRMContext';
 
 
-function Facturas(){
+function Facturas() {
 
     const navigate = useNavigate();
 
     //Trabajar con useState
     const [facturas, guardarfacturas] = useState([]);
 
-      // utilizar valores del context
-      const [auth, guardarAuth ] = useContext( CRMContext );
+    // utilizar valores del context
+    const [auth, guardarAuth] = useContext(CRMContext);
 
-        // use effect es similar a componentdidmount y willmount
-        useEffect( () => {
+    // use effect es similar a componentdidmount y willmount
+    useEffect(() => {
 
-            if(auth.token !== '') {
-                // Query a la API
-                const consultarAPI = async () => {
-                    try {
-                        const facturaConsulta = await clienteAxios.get('/factura', {
-                            headers: {
-                                Authorization : `Bearer ${auth.token}`
-                            }
-                        });
-        
-                        // colocar el resultado en el state
-                        guardarfacturas(facturaConsulta.data);
-    
-                    } catch (error) {
-                        // Error con authorizacion
-                        if(error.response.status === 500) {
-                            navigate('/iniciar-sesion');
+        if (auth.token !== '') {
+            // Query a la API
+            const consultarAPI = async () => {
+                try {
+                    const facturaConsulta = await clienteAxios.get('/factura', {
+                        headers: {
+                            Authorization: `Bearer ${auth.token}`
                         }
+                    });
+
+                    // colocar el resultado en el state
+                    guardarfacturas(facturaConsulta.data);
+
+                } catch (error) {
+                    // Error con authorizacion
+                    if (error.response.status === 500) {
+                        navigate('/iniciar-sesion');
                     }
                 }
-                consultarAPI();
-            } else {
-                navigate('/iniciar-sesion');
             }
-        }, [] ); //[facturas] es para refrescar buscar otra manera
-    
-    
-        // Si el state esta como false
-        if(!auth.auth) {
-            navigate('/iniciar-sesion'); //REVISAR
+            consultarAPI();
+        } else {
+            navigate('/iniciar-sesion');
         }
+    }, []); //[facturas] es para refrescar buscar otra manera
 
-    return(
+
+    // Si el state esta como false
+    if (!auth.auth) {
+        navigate('/iniciar-sesion'); //REVISAR
+    }
+
+    return (
         <Fragment>
-            <h2>Facturas</h2>
+            <Box pl={1} pr={1} sx={{ display: { xs: 'flex', md: 'none' }, flexDirection: 'column' }}>
+                <Typography variant="h4" gutterBottom component="h2"
+                    sx={{
+                        mr: 2,
+                        fontWeight: 700,
+                        letterSpacing: '.3rem',
+                        color: 'white',
+                        paddingTop: '2rem',
+                        paddingBottom: '1rem'
+                    }}
+                >
+                    Facturas
+                </Typography>
 
-            <Link to={"/factura/nuevo"} className="btn btn-verde nvo-cliente"> 
+                <Button sx={{ alignSelf: 'flex-start' }} variant="contained" component={Link} to={"/factura/nuevo"} color="secondary" startIcon={<AddCircleOutlinedIcon />}>
+                    Nueva factura
+                </Button>
+            </Box>
+            {/* <h2>Facturas</h2> */}
+
+            {/* <Link to={"/factura/nuevo"} className="btn btn-verde nvo-cliente">
                 <i className="fas fa-plus-circle"></i>
                 Nueva factura
-            </Link>
+            </Link> */}
 
-            <ul className="listado-clientes">
+            {/* <ul className="listado-clientes">
                 {facturas.map(factura => (
-                    <Factura 
+                    <Factura
                         key={factura.idFactura}
                         factura={factura}
+                        card={true}
                     />
-                ) )}
-            </ul>
+                ))}
+            </ul> */}
+            <Box sx={{ width: '100%' }}>
+                <Grid pt={1} container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2 }}>
+                    {facturas.map(factura => (
+                        <Factura
+                            key={factura.idFactura}
+                            factura={factura}
+                            card={true}
+                        />
+                    ))}
+
+                </Grid>
+
+            </Box>
         </Fragment>
     )
 }
