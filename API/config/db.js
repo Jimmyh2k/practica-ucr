4bf0d6147fdf5f6b6cdb2a238e0ac6b6e58732e2
Braulio <braulio@focusgte.com>
2022-07-05 20:05:44 -0600

    Cambio de nombre de la API

--- API/config/db.js
+++ API/config/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize('pruebacliente', 'root', 'root', {
+const db = new Sequelize('hoteljavy', 'root', 'root', {
   host: '127.0.0.1',
   dialect: 'mysql',
   port: '3306',
